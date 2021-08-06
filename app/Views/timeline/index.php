cb1c98538de8816e3968defe7f10cc2925780c99
topcoder1208 <topcoder1208@gmail.com>
2021-08-06 10:00:29 -0700

    update width of timeline

--- app/Views/timeline/index.php
+++ app/Views/timeline/index.php
@@ -1,6 +1,6 @@
 <div class="box">
     <div class="box-content">
-        <div id="timeline-content" class="page-wrapper clearfix mb20 <?php if(isset($department_id) && $department_id){echo "col-md-9 m-auto";} ?>">
+        <div id="timeline-content" class="page-wrapper clearfix mb20 <?php if(!isset($department_id) || !$department_id){echo "col-md-9 m-auto";} ?>">
             <?php echo view("timeline/post_form", array('department_id'=>(isset($department_id) ? $department_id : ''))); ?>
             <?php echo timeline_widget(array("limit" => 20, "offset" => 0, "is_first_load" => true, 'department_id'=>(isset($department_id) ? $department_id : ''))); ?>
         </div>
